004d6af775b413538ce23fc3ec2fcd87c1047394
Alina Lipova <alina.ag@gmail.com>
2021-08-14 20:11:21 +0500

    payment insert

--- Web_Project_CoursesInfo/CoursesInfo/insert_java.php
+++ Web_Project_CoursesInfo/CoursesInfo/insert_java.php
@@ -6,18 +6,22 @@ $user = "u_19_lag";
 $password = "123";
 $conn = pg_connect("host=159.69.151.133 port=5056 dbname=db_19_lag user=u_19_lag password=123");
 $query = "WITH rows AS (
-    INSERT INTO public.students
-        (name, surname, email, country)
-    VALUES
-        ('$_POST[name]','$_POST[surname]',
-        '$_POST[email]','$_POST[country]')
-    RETURNING student_id, name, surname
-)
-
-INSERT INTO public.students_courses (student_id, name, surname, name_course)
-    SELECT student_id, name, surname, 'Java'
-    FROM rows
-    RETURNING student_id, name, surname;";
+        INSERT INTO public.students
+            (name, surname, email, country)
+        VALUES
+            ('$_POST[name]','$_POST[surname]',
+            '$_POST[email]','$_POST[country]')
+        RETURNING student_id, name, surname
+    ),
+    rows1 AS (INSERT INTO public.students_courses
+          (student_id, name, surname, name_course)
+        SELECT student_id, name, surname, 'Java'
+        FROM rows
+        RETURNING student_id, name, surname)
+    INSERT INTO public.payments (student_id, name, surname, name_course, payment)
+            SELECT student_id, name, surname, 'Java', '0'
+            FROM rows1
+            RETURNING student_id, name, surname, name_course;";
 pg_query($conn, $query);
 header("Location: $_SERVER[HTTP_REFERER]");
 ?>
